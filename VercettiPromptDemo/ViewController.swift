54ee16f72c851b6387ae6624f9604d369f8e622b
Akash Malhotra <akash.m@servify.in>
2018-10-06 14:52:40 +0530

    Initial Commit

---
+++ VercettiPromptDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VercettiPromptDemo
+//
+//  Created by Akash Malhotra on 06/10/18.
+//  Copyright © 2018 Akashvercetti Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
